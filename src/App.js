ea264e1df37ef17cc9b24fdc44a12b89767a0075
odikadzegiorgi4@gmail.com <odikadzegiorgi4@gmail.com>
2023-04-07 14:14:18 +0400

    homework3react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,73 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
+const randomUsers =[
+  {
+    userName: "name1",
+    id: 1
+  },
+  {
+    userName: "name2",
+    id: 2
+  },
+  {
+    userName: "name3",
+    id: 3
+  },
+  {
+    userName: "name4",
+    id: 4
+  },
+  {
+    userName: "name5",
+    id: 5
+  },
+  {
+    userName: "name6",
+    id: 6
+  },
+  {
+    userName:" name7",
+    id: 7
+  },
+  {
+    userName: "name8",
+    id: 8
+  },
+  {
+    userName: "name9",
+    id: 9
+  },
+  {
+    userName: "name10",
+    id: 10
+  },
+]
+
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [users, setUsers] = useState([])
+
+  useEffect(()=>{
+    setUsers(randomUsers)
+  }, [])
+  
+  const removeUser = () =>{
+    setUsers((prev)=>{
+      const index = Math.floor(Math.random() * prev.length)
+      const newUsers = prev.filter((_,ind)=> ind !== index)
+      return newUsers
+    })
+  }
+
+  return(
+    <div>
+      <div>
+        {users.map(({id, userName})=>{
+          return <h2 key={id}>{userName}</h2>
+        })}
+        <button onClick={removeUser}> remove User</button>
+      </div>
     </div>
   );
 }
